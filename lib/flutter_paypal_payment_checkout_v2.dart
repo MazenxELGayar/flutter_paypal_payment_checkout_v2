07920a32107efc478c3c4aa51353404414b36aae
MazenxELGayar <56363166+MazenxELGayar@users.noreply.github.com>
2025-12-10 05:17:59 +0200

    exported rest of models

--- lib/flutter_paypal_payment_checkout_v2.dart
+++ lib/flutter_paypal_payment_checkout_v2.dart
@@ -14,7 +14,31 @@ export 'src/models/shared_models.dart';
 // ----------------------
 export 'src/v1/paypal_service_v1.dart';
 
+// V1 Enums
+export 'src/v1/enums/pay_pal_allowed_payment_method_v1.dart';
+export 'src/v1/enums/pay_pal_order_intent_v1.dart';
+
+// V1 Models
+export 'src/v1/models/paypal_transaction_v1.dart';
+export 'src/v1/models/paypal_transaction_v1_amount.dart';
+export 'src/v1/models/paypal_transaction_v1_item.dart';
+export 'src/v1/models/pay_pal_order_request_v1.dart';
+export 'src/v1/models/pay_pal_shipping_address_v1.dart';
+
 // ----------------------
 // V2 API
 // ----------------------
 export 'src/v2/pay_pal_service_v2.dart';
+
+// V2 Enums
+export 'src/v2/enums/pay_pal_item_category_v2.dart';
+export 'src/v2/enums/pay_pal_landing_page_v2.dart';
+export 'src/v2/enums/pay_pal_order_intent_v2.dart';
+export 'src/v2/enums/pay_pal_payment_method_preference_v2.dart';
+export 'src/v2/enums/pay_pal_shipping_preference_v2.dart';
+export 'src/v2/enums/pay_pal_user_action_v2.dart';
+
+// V2 Models
+export 'src/v2/models/pay_pal_experience_context_v2.dart';
+export 'src/v2/models/pay_pal_order_request_v2.dart';
+export 'src/v2/models/pay_pal_purchase_unit_v2.dart';
